c1f50baf3bc4f311c88b4895f8b4cba142a74224
Hideki Saito <hidekis@gmail.com>
2026-04-20 19:34:41 -0700

    Bump version to 1.2.1 in README, __init__.py, and blender_manifest.toml

--- beat_flipper_driver/__init__.py
+++ beat_flipper_driver/__init__.py
@@ -4,7 +4,7 @@
 bl_info = {
     "name": "Beat Flipper Driver",
     "author": "Hideki Saito",
-    "version": (1, 2, 0),
+    "version": (1, 2, 1),
     "blender": (3, 0, 0),
     "location": "View3D > Sidebar > Beat Flip",
     "description": "Adds BPM-based custom-property drivers to selected objects",
